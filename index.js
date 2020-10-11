b3fe335d3d9731be0c580885df4cf9900dfb6693
Andrew Smithson <smithson@uk.ibm.com>
2020-10-11 21:24:45 +0100

    Add support for self-hosted blogs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const wpcom = require('wpcom')(process.env.OAuthToken);
+const wpapi = require('wpapi');
 const queryString = require('query-string');
 
 exports.handler = async function(event) {
@@ -6,7 +7,17 @@ exports.handler = async function(event) {
     const message = queryString.parse(Buffer.from(event.body, 'base64').toString());
     //Create a post on the blog with the SMS message
     //Use the message text as the blog title and content
-    return wpcom.site( process.env.BlogEndpoint )
-                .addPost( { title: message.Body,
-                            content: message.Body } );
+    if(process.env.SelfHosted){
+        const wp = new wpapi(process.env.BlogEndpoint);
+        return wp.posts().create({
+            title: message.Body,
+            content: message.Body
+        });
+    } else {
+        return wpcom.site(process.env.BlogEndpoint)
+                    .addPost({
+                        title: message.Body,
+                        content: message.Body 
+                    });
+    }
 };
